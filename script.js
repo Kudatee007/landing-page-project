50378ed313c9485a003b82580c79b31b4e5b30bc
kudaT <timilehinkudaisi01@gmail.com>
2025-02-13 01:12:48 +0100

    feat: Added scroll-to-top button with dynamic visibility on scroll

--- script.js
+++ script.js
@@ -4,11 +4,33 @@ const hamburgerBtn = document.querySelector(".hamburger-btn");
 const icon = document.getElementById("icon");
 const footerLogo = document.getElementById("footer-logo");
 const headerLogo = document.getElementById("header-logo");
+let scrollTop = document.getElementById("scroll-top");
 
 // dark mode toggle
 toggleBtn.onclick = function () {
   dropDownMenu.classList.toggle("open");
 };
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    scrollTop.style.display = "block";
+  } else {
+    scrollTop.style.display = "none";
+  }
+}
+
+console.log(scrollTop);
+
+// When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
 
 // function to update theme
 icon.onclick = function () {
